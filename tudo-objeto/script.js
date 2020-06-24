fa4ed0c0bfdb5733642363e6b50529eb33bd49e5
Gustavo Castro <gustavocastrocs@gmail.com>
2020-06-24 08:05:26 -0300

    tudo obejto

---
+++ tudo-objeto/script.js
@@ -0,0 +1,22 @@
+//Tudo e objeto: strings, numeros, boolean, objetos e mais.
+
+
+//Exercicios
+
+//Nomeie 3 propriedades ou metodos de strings
+
+var nome = 'Gustavo';
+
+console.log(nome.toLocaleLowerCase());
+console.log(nome.toLocaleUpperCase());
+console.log(nome.length);
+
+//Nomeie 5 propriedades ou metodos de elementos do DOM
+
+var btn = document.querySelector('.btn');
+// addEventListener
+// appendChild
+// innerHTML
+// id
+// outerHTML
+console.log(btn);
